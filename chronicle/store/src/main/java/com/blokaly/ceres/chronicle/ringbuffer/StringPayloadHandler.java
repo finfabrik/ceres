c097baba34b83a8967ebb5ca10a6f90571840dd3
Charlie Zhang <blokaly@users.noreply.github.com>
2018-07-30 09:51:43 +0800

    ignore store persisting if queue closed

--- chronicle/store/src/main/java/com/blokaly/ceres/chronicle/ringbuffer/StringPayloadHandler.java
+++ chronicle/store/src/main/java/com/blokaly/ceres/chronicle/ringbuffer/StringPayloadHandler.java
@@ -6,15 +6,17 @@ import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 
 public class StringPayloadHandler implements EventHandler<StringPayload> {
 
+  private final SingleChronicleQueue queue;
   private final ExcerptAppender appender;
 
   public StringPayloadHandler(SingleChronicleQueue queue) {
+    this.queue = queue;
     this.appender = queue.acquireAppender();
   }
 
   @Override
   public void onEvent(StringPayload event, long sequence, boolean endOfBatch) throws Exception {
-    if (event != null) {
+    if (event != null && !queue.isClosed()) {
       event.writeBytes(appender);
     }
   }
