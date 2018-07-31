f68991009681bebbf9c843f2e48047b00a725079
Charlie Zhang <blokaly@users.noreply.github.com>
2018-07-31 16:42:40 +0800

    subscribe to bitmex trades data

--- gateways/bitmex/src/main/java/com/blokaly/ceres/bitmex/event/Subscribe.java
+++ gateways/bitmex/src/main/java/com/blokaly/ceres/bitmex/event/Subscribe.java
@@ -4,21 +4,16 @@ import java.util.Collection;
 
 public class Subscribe {
   private static final String orderbook = "orderBookL2:";
+  private static final String liveTrade = "trade:";
   private final String op = "subscribe";
   private final String[] args;
 
-  public Subscribe(String... symbols) {
-    args = new String[symbols.length];
-    for (int i = 0; i < symbols.length; i++) {
-      args[i] = orderbook + symbols[i].toUpperCase();
-    }
-  }
-
   public Subscribe(Collection<String> symbols) {
-    args = new String[symbols.size()];
+    args = new String[2 * symbols.size()];
     int idx = 0;
     for (String symbol : symbols) {
       args[idx++] = orderbook + symbol.toUpperCase();
+      args[idx++] = liveTrade + symbol.toUpperCase();
     }
   }
 }
