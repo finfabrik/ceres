5c49072d82c4a0424b1e99b23fe7ab5d2b3e4122
Charlie Zhang <blokaly@users.noreply.github.com>
2018-08-31 15:21:03 +0800

    added tradeId column

--- gateways/binance/src/main/java/com/blokaly/ceres/binance/TradesHandler.java
+++ gateways/binance/src/main/java/com/blokaly/ceres/binance/TradesHandler.java
@@ -13,7 +13,8 @@ public class TradesHandler {
   private static final String SYMBOL_COL = "symbol";
   private static final String PRICE_COL = "price";
   private static final String SIZE_COL = "size";
-  private static final String TRADE_TIME_COL = "tradeId";
+  private static final String TRADE_TIME_COL = "tradeTime";
+  private static final String TRADE_ID_COL = "tradeId";
   private static final String MAKER_COL = "buyerIsMaker";
   private final PairSymbol pair;
   private final BatchedPointsPublisher publisher;
@@ -28,23 +29,24 @@ public class TradesHandler {
     if (pair.getCode().equalsIgnoreCase(trade.getSymbol())) {
       publisher.publish(builder -> {
         String symbol = pair.toPairString();
-        buildPoint(trade.getTime(), symbol, trade.getPrice().asDbl(), trade.getQuantity().asDbl(), trade.getTradeTime(), trade.isBuyerMarketMaker(), builder);
+        buildPoint(trade.getTime(), symbol, trade.getPrice().asDbl(), trade.getQuantity().asDbl(), trade.getTradeTime(), trade.getTradeId(), trade.isBuyerMarketMaker(), builder);
       });
     }
   }
 
   public void publishOpen() {
     publisher.publish(builder -> {
-      buildPoint(System.currentTimeMillis(), pair.toPairString(), 0D, 0D, 0L, false, builder);
+      buildPoint(System.currentTimeMillis(), pair.toPairString(), 0D, 0D, 0L, 0L, false, builder);
     });
   }
 
-  private void buildPoint(long time, String symbol, double price, double size, long tradeTime, boolean isBuyerMaker, PointBuilderFactory.BatchedPointBuilder builder) {
+  private void buildPoint(long time, String symbol, double price, double size, long tradeTime, long tradeId, boolean isBuyerMaker, PointBuilderFactory.BatchedPointBuilder builder) {
     builder.measurement(MEASUREMENT).time(time, TimeUnit.MILLISECONDS);
     builder.tag(SYMBOL_COL, symbol.toUpperCase());
     builder.addField(PRICE_COL, price);
     builder.addField(SIZE_COL, size);
     builder.addField(TRADE_TIME_COL, tradeTime);
+    builder.addField(TRADE_ID_COL, tradeId);
     builder.addField(MAKER_COL, isBuyerMaker);
   }
 }
